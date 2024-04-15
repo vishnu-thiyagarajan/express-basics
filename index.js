54074a0c0beae34203bf5dcc5baaed797c0b07f2
unknown <admin@JS-L2-23.sce.com>
2024-04-15 10:58:14 +0530

    jwt sign and verify

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+console.log(process.env.JWT_SECRET)
 const express = require('express')
 const birds = require('./routes/birds')
 const app = express()
--- routes/birds.js
+++ routes/birds.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken');
+
+router.post('/login', (req, res) => {
+  console.log(req.body);
+  //check username password matches with db 
+  const token = jwt.sign(
+    { id: '123', name: 'test user' }, process.env.JWT_SECRET);
+  res.send(token)
+})
 
 router.get('/', (req, res) => {
   res.send('Birds home page')
 })
-
-router.post('/about', (req, res) => {
-  console.log(req.body);
-  res.send('About birds')
-})
-
 module.exports = router
