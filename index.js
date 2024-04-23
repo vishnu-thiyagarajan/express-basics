8d7eee74be3939b116349073e42e36300891ded4
unknown <admin@JS-L2-23.sce.com>
2024-04-23 12:46:10 +0530

    connect mongodb

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 require('dotenv').config()
 console.log(process.env.JWT_SECRET)
 const express = require('express')
+const mongoose = require('mongoose')
+const MONGOURL = process.env.MONGO_URL;
 const birds = require('./routes/birds')
 const app = express()
+mongoose.connect(MONGOURL).then(() => {
+    console.log("Database connected successfully.");
+    app.listen(3000, () => {
+      console.log(`Server is running on port 3000`);
+    });
+  });
 
-app.listen(3000, ()=>{
-    console.log("Starting the server")
-})
+const userSchema = new mongoose.Schema({
+    prodID: Number,
+    price: Number,
+    quantity: Number,
+  });
+const UserModel = mongoose.model("users", userSchema);
+app.get("/getUsers", async (req, res) => {
+const userData = await UserModel.find();
+res.json(userData);
+});
 app.use(express.json())
 app.use('/birds', birds)
 // const middleware = (req, res, next)=>{
--- routes/birds.js
+++ routes/birds.js
@@ -9,8 +9,14 @@ router.post('/login', (req, res) => {
     { id: '123', name: 'test user' }, process.env.JWT_SECRET);
   res.send(token)
 })
-
-router.get('/', (req, res) => {
+const verifyToken = (req, res, next) => {
+  const token = req?.headers?.authorization?.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET,(err, result)=>{
+    if (result) next();
+    else res.send(err);
+  })
+}
+router.get('/', verifyToken, (req, res) => {
   res.send('Birds home page')
 })
 module.exports = router
